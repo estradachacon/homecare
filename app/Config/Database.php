7ad360a148b13e5f178e776ee9f0b8d86380ced3
estradachacon <cestrada_chacon@outlook.com>
2025-11-07 13:48:14 -0600

    incluye revisión horaria ·4

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'carytel_fcdb',
+        'password'     => 'Aleluya:_3',
+        'database'     => 'carytel_fcdb',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
